4d274c1b09766f15016adcdf64b6acdfcaea58f8
Richard Bell <richard.t.bell90@gmail.com>
2017-12-06 11:25:16 +0000

    Finish assignment 3

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import './Courses.css';
+import {Link, Route} from 'react-router-dom'
+import Course from '../Course/Course'
 
 class Courses extends Component {
     state = {
@@ -18,10 +20,16 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
+                            return (
+                                        <Link to={'/courses/' + course.id + '/' + course.title} >
+                                            <article className="Course" key={course.id}>{course.title}</article>;
+                                        </Link>
+                                    )
+                                    
                         } )
                     }
                 </section>
+                <Route path={this.props.match.url + "/:id/:title"} component={Course}/>
             </div>
         );
     }
