45d4953005e377a8fb7d87586b5b3720d4f79a03
Richard Bell <richard.t.bell90@gmail.com>
2017-12-06 10:37:46 +0000

    lecture 199

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,9 +8,9 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    componentDidMount () {
+    loadData () {
         if (this.props.match.params.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.match.params.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
                 axios.get('/posts/' + this.props.match.params.id).then(response => {
                     this.setState({loadedPost: response.data})
                 })
@@ -18,15 +18,23 @@ class FullPost extends Component {
         }
     }
 
+    componentDidMount () {
+        this.loadData()
+    }
+
+    componentDidUpdate() {
+        this.loadData()
+    }
+
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id).then(response => {
+        axios.delete('/posts/' + this.props.match.params.id).then(response => {
             console.log(response);
         })
     }
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.id) {
             post = <p style={{textAlign: 'center'}}>Loading...</p>;
         }
         if (this.state.loadedPost) {
